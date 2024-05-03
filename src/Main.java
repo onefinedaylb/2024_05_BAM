4bb5df95d07e8ae40fb004f0385acc085a9bd3c5
onefinedaylb <psssjp22@example.com>
2024-05-03 15:42:04 +0900

    articl write 구현중

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,40 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		int id = 1;
+		
 		while(true) {
 			System.out.println("명령어) ");
-			String cmd = sc.nextLine();
+			String cmd = sc.nextLine().trim();
+			
 
 			if (cmd.equals("exit")) {
 				break;
 			}
 			
+			if (cmd.length() == 0) {
+				System.out.println("명령어를 입력해주세요");
+				continue;
+			}
+			
+			if (cmd.equals("article list")) {
+				System.out.println("게시글이 없습니다");
+				
+			} else if (cmd.equals("article write")) {
+				
+				
+				System.out.print("제목 : ");
+				String title = sc.nextLine().trim();
+				System.out.print("내용 : ");
+				String body = sc.nextLine().trim();
+				System.out.println(id + "번 글이 생성되었습니다. ");
+				id++;
+
+			} else {
+				System.out.println("존재하지 않는 명령어 입니다");
+			}
+			
 		}
-		//sfklsljdlkjlkjlkasd
 		
 		sc.close();
 		
