48994d04a102ee34031a05b4331455f299878554
onefinedaylb <psssjp22@example.com>
2024-05-07 16:48:57 +0900

    article list 수정

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,10 +14,9 @@ public class Main {
 		int id = 1;
 		
 		while(true) {
-			System.out.println("명령어) ");
+			System.out.printf("명령어) ");
 			String cmd = sc.nextLine().trim();
 			
-
 			if (cmd.equals("exit")) {
 				break;
 			}
@@ -31,35 +31,31 @@ public class Main {
 					System.out.println("존재하는 게시글이 없습니다");
 					continue;
 				}
-				if (articles.size() != 0) {
-					System.out.println("번호   |   제목");
-					for (int i = articles.size() - 1; i >= 0; i--) {
-						Artixle article = articles.get(i);
-						System.out.println("%d   |   %s\n", article.id, article.title);
-					}
-					
+				
+				System.out.println("번호	|	제목");
+				
+				for (int i = articles.size() - 1; i  >= 0; i--) {
+					Article article = articles.get(i);
+					System.out.printf("%d	|	%s\n", article.id, article.title);
 				}
 				
 			} else if (cmd.equals("article write")) {
-				
-				
-				System.out.print("제목 : ");
+				System.out.printf("제목 : ");
 				String title = sc.nextLine().trim();
-				System.out.print("내용 : ");
+				System.out.printf("내용 : ");
 				String body = sc.nextLine().trim();
 				
-//				Article article = new Article(id, title, body);
-			
-//				articles.add(article);
+				Article article = new Article(id, title, body);
+				articles.add(article);
 				
-				articles.add(new Article(id, title, body));
-				
-				System.out.println(id + "번 글이 생성되었습니다. ");
+				System.out.println(id + "번 글이 생성되었습니다");
 				id++;
-
+				
 			} else {
+			
 				System.out.println("존재하지 않는 명령어 입니다");
 			}
+			
 			
 		}
 		
@@ -68,7 +64,6 @@ public class Main {
 		System.out.println("== 프로그램 끝 ==");
 	}
 }
-
 
 class Article {
 	int id;
@@ -79,6 +74,5 @@ class Article {
 		this.id = id;
 		this.title = title;
 		this.body = body;
-		
 	}
 }
